dffb04b76ed51f79ec92db0fa8fff7c1334087a3
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-02-25 18:39:51 -0300

    C04 - Manipulando o DOM

--- Projeto-Calculadora/scripts/controller/CalcController.js
+++ Projeto-Calculadora/scripts/controller/CalcController.js
@@ -2,7 +2,18 @@ class CalcController {
 
     constructor(){
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML ="4567";
+        dateEl.innerHTML = "01/02/2021";
+        timeEl.innerHTML = "00:00";
     }
 
     get displayCalc(){
@@ -13,12 +24,12 @@ class CalcController {
         this._displayCalc = valor;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return this._currentDate;
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 
 }
