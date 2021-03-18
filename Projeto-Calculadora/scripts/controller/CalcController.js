72cddab6e49c0210027a5210ac1ab8bba13dda07
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-03-17 22:08:20 -0300

    C15 - Botão Ponto

--- Projeto-Calculadora/scripts/controller/CalcController.js
+++ Projeto-Calculadora/scripts/controller/CalcController.js
@@ -36,7 +36,8 @@ class CalcController {
 
     clearAll(){
         this._operation = [];
-
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
     }
 
@@ -137,17 +138,15 @@ class CalcController {
     }
 
     addOperation(value){
+
         if(isNaN(this.getLastOperation())){
             
             if(this.isOperator(value)){
 
                 this.setLastOperation(value);
 
-            }else if(isNaN(value)){
-                
-                console.log("Outra coisa");
-
             }else {
+
                 this.pushOperation(value);
 
                 this.setLastNumberToDisplay();
@@ -162,7 +161,7 @@ class CalcController {
             }else {
                 
                 let newValue = this.getLastOperation().toString() + value.toString(); 
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
                 
                 this.setLastNumberToDisplay();
             }
@@ -171,9 +170,21 @@ class CalcController {
     }
 
     setError(){
+
         this.displayCalc = "Error";
+
     }
 
+    addDot(){
+        let lastOperation = this.getLastOperation();
+
+        if(this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+        this.setLastNumberToDisplay();
+    }
     execBtn(value) {
 
         switch(value) {
@@ -186,7 +197,6 @@ class CalcController {
             case 'soma':
                 this.addOperation('+');
                 break;
-
             case 'subtracao':
                 this.addOperation('-');
                 break;
@@ -203,7 +213,7 @@ class CalcController {
                 this.calc();
                 break;
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
                 break;
             case '0':
             case '1':
