e65aa8b70d93858ece8e9d062125fdc6d16bf7dc
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-03-24 22:43:19 -0300

    C20 - Definir o valor máximo

--- Projeto-Calculadora/scripts/controller/CalcController.js
+++ Projeto-Calculadora/scripts/controller/CalcController.js
@@ -389,6 +389,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if(value.toString().length > 10){
+            this.setError();
+            return;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
