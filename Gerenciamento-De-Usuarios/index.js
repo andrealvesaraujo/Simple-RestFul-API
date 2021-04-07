0275e96dce00960e10332cdde731b182eff60043
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-06 23:03:46 -0300

    G6 - IF e ELSE

--- Gerenciamento-De-Usuarios/index.js
+++ Gerenciamento-De-Usuarios/index.js
@@ -11,6 +11,15 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 
 fields.forEach(function(field,index){
 
-    console.log(field.id, field.name, field.value, field.checked, index );
+    if(field.name == "gender"){
+        if(field.checked){
+            console.log("Sim",field);
+        }
+
+    }else{
+        console.log("Não");
+    }
+
+    // console.log(field.id, field.name, field.value, field.checked, index );
 
 });
