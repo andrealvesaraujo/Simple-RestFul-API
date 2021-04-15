e7ff6b2a579ff941dda8e68360d4688dd1f14bf9
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-15 14:35:03 -0300

    G18 - Validando Formulários

--- Gerenciamento-De-Usuarios/controllers/UserController.js
+++ Gerenciamento-De-Usuarios/controllers/UserController.js
@@ -79,7 +79,15 @@ class UserController {
 
         let user = {};
 
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function(field,index) {
+
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
             if (field.name == "gender") {
     
@@ -101,6 +109,10 @@ class UserController {
     
         });
     
+        if(!isValid) {
+            return false;
+        }
+
         return new User(
             user.name, 
             user.gender, 
