78beb0964fba6d06394b6c817df10ec54c14503e
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-19 11:46:06 -0300

    G28 - Armazenando Dados com localStorage

--- Gerenciamento-De-Usuarios/controllers/UserController.js
+++ Gerenciamento-De-Usuarios/controllers/UserController.js
@@ -212,9 +212,9 @@ class UserController {
 
         let users = [];
 
-        if(sessionStorage.getItem("users")){
+        if(localStorage.getItem("users")){
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
 
         }
 
@@ -245,7 +245,8 @@ class UserController {
 
         users.push(data);
 
-        sessionStorage.setItem("users",JSON.stringify(users));
+        // sessionStorage.setItem("users",JSON.stringify(users));
+        localStorage.setItem("users",JSON.stringify(users));
 
     }
 
