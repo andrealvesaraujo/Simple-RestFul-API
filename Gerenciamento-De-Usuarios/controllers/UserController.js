b112df33fefe863f7e61c5ce436ed597b306a15a
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-04-19 12:04:17 -0300

    G29 - Refactoring - Otimizando o Template

--- Gerenciamento-De-Usuarios/controllers/UserController.js
+++ Gerenciamento-De-Usuarios/controllers/UserController.js
@@ -50,23 +50,13 @@ class UserController {
 
                         result._photo = content;
 
-                    }                    
-
-                    tr.dataset.user = JSON.stringify(result);
-
-                    tr.innerHTML = `
-                        <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${result._name}</td>
-                        <td>${result._email}</td>
-                        <td>${(result._admin) ? 'Sim' : 'Não' }</td>
-                        <td>${Utils.dateFormat(result._register)}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                    `;
+                    }  
                     
-                    this.addEventsTr(tr);
+                    let user = new User();
+
+                    user.loadFromJSON(result);
+                    
+                    this.getTr(user, tr);
 
                     this.updateCount();
 
@@ -252,7 +242,17 @@ class UserController {
 
     addLine(dataUser) {
 
-        let tr = document.createElement('tr');
+        let tr = this.getTr(dataUser);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
+    }
+
+    getTr(dataUser, tr = null){
+
+        if(tr === null) tr = document.createElement('tr');
 
         tr.dataset.user = JSON.stringify(dataUser);
 
@@ -270,10 +270,7 @@ class UserController {
 
         this.addEventsTr(tr);
 
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
-
+        return tr;
     }
 
     addEventsTr(tr){
